de0a3e431653677e6abd09ece93505d7111c199b
Sumit Bhardwaj <sumitbhardwaj78368@gmail.com>
2025-09-04 11:34:55 +0530

    style: update Background component UI

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,9 +1,9 @@
 
 const Background = () => {
     return (
-        <div>
+        <div className="">
             {/* BACKGROUND  */}
-            <div className=' absolute left-[50%] -translate-x-1/2 top-[7%]'>
+            <div className=' z-0 absolute left-[50%] -translate-x-1/2 top-[7%]'>
                 <h4 className=' text-lg font-semibold'>Documents</h4>
             </div>
             <div className=' absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
