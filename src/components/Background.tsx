cf0103171b6100e9f833fa5a53f33d6e5638efa5
Sumit Bhardwaj <sumitbhardwaj78368@gmail.com>
2025-09-04 10:25:16 +0530

    feat: add Background component

---
+++ src/components/Background.tsx
@@ -0,0 +1,16 @@
+
+const Background = () => {
+    return (
+        <div>
+            {/* BACKGROUND  */}
+            <div className=' absolute left-[50%] -translate-x-1/2 top-[7%]'>
+                <h4 className=' text-lg font-semibold'>Documents</h4>
+            </div>
+            <div className=' absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+                <h2 className=' font-semibold text-[13rem]'>Docs.</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Background
