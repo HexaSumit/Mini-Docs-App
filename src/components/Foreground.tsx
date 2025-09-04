652ab1f2ab3eb9159347d79c65d3cad9864a999c
Sumit Bhardwaj <sumitbhardwaj78368@gmail.com>
2025-09-04 23:57:48 +0530

    feat: add static DataItem array and pass it as props to Card component

--- src/components/Foreground.tsx
+++ src/components/Foreground.tsx
@@ -1,11 +1,34 @@
 import Card from "./Card"
+import type { DataItem } from "../types/DataTypes"
 
 const Foreground = () => {
+    const data:DataItem[]=[
+        {
+           desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing.' ,
+           filesize:'.9Mb',
+           close:true, 
+           tag:{isOpen:true,tagTitle:'Download Now',tagColor:'green'}
+        },
+        {
+           desc: 'This is a second card and this is also draggable' ,
+           filesize:'0.4Mb',
+           close:false, 
+           tag:{isOpen:false,tagTitle:'Download Now',tagColor:'green'}
+        },
+        {
+           desc: 'This is third Card.This one is also draggable Check by moving the Card ' ,
+           filesize:'.5Mb',
+           close:true, 
+           tag:{isOpen:true,tagTitle:'Download Now',tagColor:'blue'}
+        },
+    ]
     return (
         <div>
             {/* FOREGROUND COMP */}
-            <div className=' absolute inset-0 bg-gray-300/70 z-10 p-2'>
-                <Card />
+            <div className=' absolute inset-0 z-10 p-2'>
+                <div className=" flex gap-5 flex-wrap">
+                    {data.map((item,index)=>(<Card data={item} key={index}/>))}
+                </div>
             </div>
         </div>
     )
