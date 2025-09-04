1e3f1e6232789b6e0a066ec19db1414ab54083cd
Sumit Bhardwaj <sumitbhardwaj78368@gmail.com>
2025-09-04 23:59:50 +0530

    feat: render Card component UI based on DataItem props

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,17 @@
 import { FaRegFileAlt } from "react-icons/fa";
 import { RxCross2 } from 'react-icons/rx';
+import { MdOutlineDownload } from "react-icons/md";
+import type { DataItem } from "../types/DataTypes";
 
-const Card = () => {
+interface CardProps{
+    data:DataItem;
+}
+
+const Card = ({ data }:CardProps) => {
     return (
-        <div>
+        <div >
             {/* Card Component */}
-            <div className="w-48 h-56 rounded-3xl bg-white shadow-md flex flex-col overflow-hidden">
+            <div className="w-48 h-56 rounded-3xl bg-gray-100 shadow-md flex flex-col overflow-hidden m-3">
                 {/* FILE ICON */}
                 <div className="px-4 pt-4">
                     <FaRegFileAlt className="text-lg" />
@@ -14,34 +20,28 @@ const Card = () => {
                 {/* STATIC TEXT */}
                 <div className="px-4 mt-3 flex-1">
                     <p className="text-sm text-gray-800 leading-snug">
-                        Lorem ipsum dolor sit amet consectetur adipisicing.
+                        {data.desc}
                     </p>
                 </div>
 
                 {/* FOOTER */}
                 <div className="px-4 mt-4 mb-2 flex justify-between items-center">
-                    <span className="font-semibold text-sm text-gray-700">1.4 Mb</span>
+                    <span className="font-semibold text-sm text-gray-700">{data.filesize}</span>
 
                     {/* Close Button */}
-                    <span className="w-6 h-6 flex items-center justify-center rounded-full bg-gray-100 hover:bg-gray-200 cursor-pointer transition">
-                        <RxCross2 className="text-gray-600 text-sm" />
+                    <span className="w-6 h-6 flex items-center justify-center rounded-full bg-gray-200 hover:bg-gray-200 cursor-pointer transition">
+                        {data.close ? <RxCross2 className="text-gray-600 text-sm" /> : <MdOutlineDownload className="text-gray-600 text-sm" />}
                     </span>
                 </div>
 
 
-                {/* DOWNLOAD NOW BTN  */}
-                {/* <div className="h-10 bg-green-500 flex items-center justify-center text-white text-sm font-medium cursor-pointer">
-    Download Now
-  </div> */}
-
-                {/* PROGRESS BAR */}
-                <div className="h-10 bg-blue-600 rounded-b-2xl flex items-center justify-center">
-                    {/* Slim white progress track */}
-                    <div className="w-4/5 h-1 bg-white/60 rounded-full overflow-hidden">
-                        {/* Filled progress */}
-                        <div className="h-full w-1/2 bg-white rounded-full"></div>
+                {data.tag.isOpen && (
+                    //DOWNLOAD NOW BTN
+                    <div className={`h-10 ${data.tag.tagColor==='blue'?'bg-sky-500': 'bg-green-500'} flex items-center justify-center text-white text-sm font-medium cursor-pointer`}>
+                        Download Now
                     </div>
-                </div>
+                )}
+
             </div>
         </div>
     )
